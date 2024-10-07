b72913ecc227c05ac544b0c1c77f53124dfe82df
Brad Anderson <brad@fragile.co>
2024-10-07 15:56:00 -0700

    doc this abomination

--- tap_universal_file/files.py
+++ tap_universal_file/files.py
@@ -122,6 +122,8 @@ class FilesystemManager:
 
         # Only yield files when no replication key is present or when the file is newer
         # than the replication key value, as a datetime.
+        # Note: the `five_minutes_ago` variable is super-hard-coded to a Fragile use case.
+        #       This should be configurable in the future.
         five_minutes_ago = datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(minutes=5)
         for file_dict in file_dict_list:
             if starting_replication_key_value is None or (
